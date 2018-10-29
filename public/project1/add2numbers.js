6da3ebe19d1840d1845373bbbf9dcdae7e91cd7c
ajatdarojat45 <ajatdarojat45@gmail.com>
2018-10-29 08:18:12 +0700

    css grid

--- public/project1/add2numbers.js
+++ public/project1/add2numbers.js
@@ -2,7 +2,7 @@ function kalkulator() {
   let angka = document.querySelectorAll('input');
   let i1 = angka[0].value; //angka pertama
   let i2 = angka[1].value; //angka kedua
-  angka[2].value = i1 + i2;
+  angka[2].value = parseInt(i1)+parseInt(i2);
   let pesan = document.getElementById('message');
   pesan.innerHTML = "Selesai";
 }
