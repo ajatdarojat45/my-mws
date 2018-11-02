08019c82fbd8f1590f64d5310019ec23aca2c43d
Ajat Darojat <ajatdarojat45@gmail.com>
2018-11-02 18:53:02 +0700

    update project2

--- public/project2/js/maps.js
+++ public/project2/js/maps.js
@@ -1,28 +1,32 @@
-// tugas 1
-let lokasi = [-6.4576306, 106.6464973];
-let detail = "<p align='center'><b>Rumpin</b><br>Lat:-6.4576306 Long:106.6464973 <br><br>Kabupaten Bogor, Jawa Barat</p>";
-let marker = L.marker(lokasi).addTo(mymap).bindPopup(detail);
+var mymap = L.map('mapid').setView([-6.4576306, 106.6464973], 13);
 
-//  tugas 2
-let places = [
-  {
-    "lokasi": [-6.4492597, 106.6779359],
-    "detail": "<p align='center'><b>Ciseeng</b><br>Lat:-6.4492597 Long:106.6779359 <br><br>Kabupaten Bogor, Jawa Barat</p>"
-  },
-  {
-    "lokasi": [-6.4222402, 106.7325943],
-    "detail": "<p align='center'><b>Parung</b><br>Lat:-6.4222402 Long:106.6779359 <br><br>Kabupaten Bogor, Jawa Barat</p>"
-  },
-  {
-    "lokasi": [-6.4901067, 106.8306951],
-    "detail": "<p align='center'><b>Cibinong</b><br>Lat:-6.4901067 Long:106.8306951 <br><br>Kabupaten Bogor, Jawa Barat</p>"
-  },
-  {
-    "lokasi": [-6.3478915, 106.7411559],
-    "detail": "<p align='center'><b>Pamulang</b><br>Lat:-6.3478915 Long:106.7411559 <br><br>Tanggerang Selatan, Banten</p>"
-  }
-];
+// PLACE 1
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoieXVzdWZyaXphbGgiLCJhIjoiY2pubDh4ZDFtMDZqajNwbnppandja25nbCJ9.ULxqaXHPzFArFjMk7cdcxQ'
+}).addTo(mymap);
+var marker1 = L.marker([-6.4576306, 106.6464973]).addTo(mymap);
+var circle1 = L.circle([-6.4576306, 106.6464973], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(mymap);
+marker1.bindPopup("<p align='center'><img src='../project3/images/rumpin.jpg' width='100%'><br><br><b>Tempat favorit : Rumpin</b><br>Lat:-6.4576306 Long:106.6464973 <br><br> Kabupaten Bogor Jawa Barat Indonesia</p>").openPopup();
 
-for (var p of places) {
-  var markerlist = L.marker(p.lokasi).addTo(mymap).bindPopup(p.detail);
+function onMapClick(e) {
+    console.log("Peta diklik pada posisi " + e.latlng);
 }
+mymap.on('click', onMapClick);
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("Lokasi yang dipilih: " + e.latlng.toString())
+        .openOn(mymap);
+}
+mymap.on('click', onMapClick);
